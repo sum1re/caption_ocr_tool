25a277dbbe1fd0554993e152c7a9bc6f5697f231
sum1re <appjason6561@gmail.com>
2022-05-09 12:08:39 +0800

    feat(cache): add caching to the application

---
+++ src/main/java/com/neo/caption/ocr/config/CacheConfig.java
@@ -0,0 +1,27 @@
+package com.neo.caption.ocr.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        var cacheManager = new CaffeineCacheManager();
+        var caffeine = Caffeine.newBuilder()
+                .expireAfterAccess(Duration.ofMinutes(30))
+                .initialCapacity(2 << 6)
+                .maximumSize(2 << 8);
+        cacheManager.setCaffeine(caffeine);
+        return cacheManager;
+    }
+
+}
