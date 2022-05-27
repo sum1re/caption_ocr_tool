649711553838adb513e5fef66a15603e4c109442
sum1re <appjason6561@gmail.com>
2022-05-27 21:51:02 +0800

    perf: improve startup performance

--- src/main/java/com/neo/caption/ocr/COCRApplication.java
+++ src/main/java/com/neo/caption/ocr/COCRApplication.java
@@ -1,12 +1,12 @@
 package com.neo.caption.ocr;
 
+import com.neo.caption.ocr.service.LoaderService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.bytedeco.javacpp.Loader;
 import org.bytedeco.opencv.opencv_java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import javax.annotation.PostConstruct;
@@ -14,13 +14,16 @@ import javax.annotation.PostConstruct;
 @Slf4j
 @SpringBootApplication
 @EnableAsync
-@EnableAspectJAutoProxy
 @ConfigurationPropertiesScan("com.neo.caption.ocr.property")
+@RequiredArgsConstructor
 public class COCRApplication {
+
+    private final LoaderService loaderService;
 
     @PostConstruct
     public void init() {
-        Loader.load(opencv_java.class);
+        loaderService.logInfo();
+        loaderService.loadLib(opencv_java.class);
     }
 
     public static void main(String... args) {
---
+++ src/main/java/com/neo/caption/ocr/service/LoaderService.java
@@ -0,0 +1,9 @@
+package com.neo.caption.ocr.service;
+
+public interface LoaderService {
+
+    void loadLib(Class<?>... classes);
+
+    void logInfo();
+
+}
