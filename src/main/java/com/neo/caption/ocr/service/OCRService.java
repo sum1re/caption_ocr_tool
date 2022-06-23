94524b3bcb14455f0c491dbd0ec65f0bf18e2244
sum1re <appjason6561@gmail.com>
2022-06-23 20:01:07 +0800

    refactor(service): refactor OCRService

--- src/main/java/com/neo/caption/ocr/service/OCRService.java
+++ src/main/java/com/neo/caption/ocr/service/OCRService.java
@@ -1,36 +1,35 @@
 package com.neo.caption.ocr.service;
 
-import com.neo.caption.ocr.domain.dto.CaptionRowDto;
-import com.neo.caption.ocr.domain.entity.CaptionRow;
 import com.neo.caption.ocr.util.OpenCVUtil;
+import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.tesseract.TessBaseAPI;
-import org.jetbrains.annotations.NotNull;
 import org.opencv.core.Mat;
 
-import java.util.ArrayList;
-import java.util.List;
+public class OCRService {
 
-public record OCRService(TessBaseAPI api) {
+    private final TessBaseAPI tessBaseAPI;
+    private BytePointer bytePointer;
 
-    private String doOCR(Mat mat) {
-        var bytes = OpenCVUtil.mat2ByteArrayByGet(mat);
-        api.SetImage(bytes, mat.cols(), mat.rows(), mat.channels(), mat.cols());
-        try (var bytePointer = api.GetUTF8Text()) {
-            return bytePointer.isNull() ? "" : bytePointer.getString();
-        }
+    public OCRService(TessBaseAPI tessBaseAPI) {
+        this.tessBaseAPI = tessBaseAPI;
+        this.bytePointer = null;
     }
 
-    public List<CaptionRowDto> doOCR(@NotNull List<CaptionRow> captionRowList) {
-        var dtoList = new ArrayList<CaptionRowDto>(captionRowList.size());
-        for (var captionRow : captionRowList) {
-            var mat = captionRow.getMat();
-            var caption = doOCR(mat);
-            OpenCVUtil.release(mat);
-            captionRow.setCaption(caption);
-            dtoList.add(captionRow.toDto());
+    public String doOCR(Mat mat) {
+        var bytes = OpenCVUtil.mat2ByteArrayByGet(mat);
+        tessBaseAPI.SetImage(bytes, mat.cols(), mat.rows(), mat.channels(), mat.cols());
+        bytePointer = tessBaseAPI.GetUTF8Text();
+        return bytePointer.isNull() ? "" : bytePointer.getString();
+
+    }
+
+    public void close() {
+        if (tessBaseAPI != null) {
+            tessBaseAPI.close();
         }
-        captionRowList.clear();
-        return dtoList;
+        if (bytePointer != null) {
+            bytePointer.close();
+        }
     }
 
 }
