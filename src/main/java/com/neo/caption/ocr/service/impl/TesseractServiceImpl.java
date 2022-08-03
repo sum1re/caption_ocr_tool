e502de653ff8893e58cef4ef666e20386a4bbee6
sum1re <appjason6561@gmail.com>
2022-08-03 13:07:22 +0800

    chore: remove unused import

--- src/main/java/com/neo/caption/ocr/service/impl/TesseractServiceImpl.java
+++ src/main/java/com/neo/caption/ocr/service/impl/TesseractServiceImpl.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bytedeco.tesseract.StringVector;
 import org.bytedeco.tesseract.TessBaseAPI;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
