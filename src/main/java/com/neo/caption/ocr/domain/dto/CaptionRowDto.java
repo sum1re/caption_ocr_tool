460fcfba985475c953b646c438986b94b03ba1ef
sum1re <appjason6561@gmail.com>
2022-08-15 10:17:07 +0800

    refactor: use BigDecimal to replace double

--- src/main/java/com/neo/caption/ocr/domain/dto/CaptionRowDto.java
+++ src/main/java/com/neo/caption/ocr/domain/dto/CaptionRowDto.java
@@ -1,4 +1,12 @@
 package com.neo.caption.ocr.domain.dto;
 
-public record CaptionRowDto(double startTime, double endTime, String caption) {
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.neo.caption.ocr.databind.TimelineSerialize;
+
+import java.math.BigDecimal;
+
+public record CaptionRowDto(
+        @JsonSerialize(using = TimelineSerialize.class) BigDecimal startTime,
+        @JsonSerialize(using = TimelineSerialize.class) BigDecimal endTime,
+        String caption) {
 }
