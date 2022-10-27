d99e4fa47ef57420e66dd0f095d2adb6eafb1098
sum1re <appjason6561@gmail.com>
2022-10-27 10:31:32 +0800

    refactor: turn bytepointer into a local variable

--- src/main/java/com/neo/caption/ocr/service/OCRService.java
+++ src/main/java/com/neo/caption/ocr/service/OCRService.java
@@ -1,34 +1,27 @@
 package com.neo.caption.ocr.service;
 
 import com.neo.caption.ocr.util.OpenCVUtil;
-import org.bytedeco.javacpp.BytePointer;
+import lombok.RequiredArgsConstructor;
 import org.bytedeco.tesseract.TessBaseAPI;
 import org.opencv.core.Mat;
 
+@RequiredArgsConstructor
 public class OCRService {
 
     private final TessBaseAPI tessBaseAPI;
-    private BytePointer bytePointer;
-
-    public OCRService(TessBaseAPI tessBaseAPI) {
-        this.tessBaseAPI = tessBaseAPI;
-        this.bytePointer = null;
-    }
 
     public String doOCR(Mat mat) {
         var bytes = OpenCVUtil.mat2ByteArrayByGet(mat);
         tessBaseAPI.SetImage(bytes, mat.cols(), mat.rows(), mat.channels(), mat.cols());
-        bytePointer = tessBaseAPI.GetUTF8Text();
-        return bytePointer.isNull() ? "" : bytePointer.getString();
-
+        var bytePointer = tessBaseAPI.GetUTF8Text();
+        var text = bytePointer.isNull() ? "" : bytePointer.getString();
+        bytePointer.close();
+        return text;
     }
 
     public void close() {
         if (tessBaseAPI != null) {
             tessBaseAPI.close();
-        }
-        if (bytePointer != null) {
-            bytePointer.close();
         }
     }
 
