63ceb12587b6c627903ed0feb8aec6b09b9d7f8d
sum1re <sum1re@mail.sum1re.xyz>
2024-01-13 10:13:34 +0800

    feat: add more flow components

--- src/main/kotlin/com/neo/caption/ocr/module/cv/CvComponent.kt
+++ src/main/kotlin/com/neo/caption/ocr/module/cv/CvComponent.kt
@@ -58,4 +58,88 @@ class MorphologyComponent : CvComponent<Morphology, Unit>() {
     override val action: Mat.() -> Unit = { this.morphology(componentParam) }
 }
 
+@LiteflowComponent("adaptiveBinarization")
+class AdaptiveBinarizationComponent : CvComponent<AdaptiveBinarization, Unit>() {
+    override val kClass = AdaptiveBinarization::class
+    override val action: Mat.() -> Unit = { this.adaptiveBinarization(componentParam) }
+}
+
+@LiteflowComponent("fixedBinarization")
+class FixedBinarizationComponent : CvComponent<FixedBinarization, Unit>() {
+    override val kClass = FixedBinarization::class
+    override val action: Mat.() -> Unit = { this.fixedBinarization(componentParam) }
+}
+
+@LiteflowComponent("bilateralFilter")
+class BilateralFilterComponent : CvComponent<BilateralFilter, Unit>() {
+    override val kClass = BilateralFilter::class
+    override val action: Mat.() -> Unit = { this.bilateralFilter(componentParam) }
+}
+
+@LiteflowComponent("boxFilter")
+class BoxFilterComponent : CvComponent<BoxFilter, Unit>() {
+    override val kClass = BoxFilter::class
+    override val action: Mat.() -> Unit = { this.boxFilter(componentParam) }
+}
+
+@LiteflowComponent("gaussianBlur")
+class GaussianBlurComponent : CvComponent<GaussianBlur, Unit>() {
+    override val kClass = GaussianBlur::class
+    override val action: Mat.() -> Unit = { this.gaussianBlur(componentParam) }
+}
+
+@LiteflowComponent("medianBlur")
+class MedianBlurComponent : CvComponent<SingleIntParam, Unit>() {
+    override val kClass = SingleIntParam::class
+    override val action: Mat.() -> Unit = { this.medianBlur(componentParam) }
+}
+
+@LiteflowComponent("hlsRange")
+class HLSRangeComponent : CvComponent<HLSRange, Unit>() {
+    override val kClass = HLSRange::class
+    override val action: Mat.() -> Unit = {
+        this.inRange(
+            componentParam.hueMin,
+            componentParam.lightnessMin,
+            componentParam.saturationMin,
+            componentParam.hueMax,
+            componentParam.lightnessMax,
+            componentParam.saturationMax
+        )
+    }
+}
+
+@LiteflowComponent("hsvRange")
+class HSVRangeComponent : CvComponent<HSVRange, Unit>() {
+    override val kClass = HSVRange::class
+    override val action: Mat.() -> Unit = {
+        this.inRange(
+            componentParam.hueMin,
+            componentParam.saturationMin,
+            componentParam.valueMin,
+            componentParam.hueMax,
+            componentParam.saturationMax,
+            componentParam.valueMax
+        )
+    }
+}
+
+@LiteflowComponent("convertColor")
+class ConvertColorComponent : CvComponent<SingleIntParam, Unit>() {
+    override val kClass = SingleIntParam::class
+    override val action: Mat.() -> Unit = { this.cvtColor(componentParam) }
+}
+
+@LiteflowComponent("convertDepth")
+class ConvertDepthComponent : CvComponent<SingleIntParam, Unit>() {
+    override val kClass = SingleIntParam::class
+    override val action: Mat.() -> Unit = { this.cvtType(componentParam) }
+}
+
+@LiteflowComponent("equalize")
+class EqualizeComponent : CvComponent<SingleIntParam, Unit>() {
+    override val kClass = SingleIntParam::class
+    override val action: Mat.() -> Unit = { this.equalizeHist(componentParam) }
+}
+
 private fun NodeComponent.context(): CvContext = this.getContextBean(CvContext::class.java)
