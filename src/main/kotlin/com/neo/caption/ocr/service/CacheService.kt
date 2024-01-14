08ba53a086ccf0ffe2c971988fb9360ae03d260e
sum1re <sum1re@mail.sum1re.xyz>
2024-01-14 15:26:32 +0800

    feat: add CacheService

---
+++ src/main/kotlin/com/neo/caption/ocr/service/CacheService.kt
@@ -0,0 +1,20 @@
+package com.neo.caption.ocr.service
+
+import com.neo.caption.ocr.common.CacheableService
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.CachePut
+import org.springframework.cache.annotation.Cacheable
+
+@CacheableService
+class CacheService {
+
+    @Cacheable(key = "#p0")
+    fun <T> get(key: String, value: T): T = value
+
+    @CachePut(key = "#p0")
+    fun <T> put(key: String, value: T): T = value
+
+    @CacheEvict(key = "#p0")
+    fun evict(key: String) = Unit
+
+}
