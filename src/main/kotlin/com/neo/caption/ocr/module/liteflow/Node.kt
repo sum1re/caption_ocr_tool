632ae938af1cb15f9d758de922ca18f2b190eedf
sum1re <sum1re@mail.sum1re.xyz>
2024-03-10 16:02:20 +0800

    feat: define node class

--- src/main/kotlin/com/neo/caption/ocr/module/liteflow/Node.kt
+++ src/main/kotlin/com/neo/caption/ocr/module/liteflow/Node.kt
@@ -48,3 +48,72 @@ data class AstModel(
     val nodeEntityList: List<NodeEntity>,
     val nodeEdgeList: List<NodeEdge>,
 ) : BaseData
+
+data class StartNode(
+    override val id: String,
+    override val name: String,
+    override val type: NodeTypeEnum = NodeTypeEnum.START,
+    override val previousList: MutableList<Node> = mutableListOf(),
+    override val nextList: MutableList<Node> = mutableListOf()
+) : Node
+
+data class EndNode(
+    override val id: String,
+    override val name: String,
+    override val type: NodeTypeEnum = NodeTypeEnum.END,
+    override val previousList: MutableList<Node> = mutableListOf(),
+    override val nextList: MutableList<Node> = mutableListOf()
+) : Node
+
+data class CommonNode(
+    override val id: String,
+    override val name: String,
+    override val type: NodeTypeEnum = NodeTypeEnum.COMMON,
+    override val previousList: MutableList<Node> = mutableListOf(),
+    override val nextList: MutableList<Node> = mutableListOf()
+) : Node
+
+data class WhenNode(
+    override val id: String,
+    override val name: String,
+    override val type: NodeTypeEnum = NodeTypeEnum.WHEN,
+    override val previousList: MutableList<Node> = mutableListOf(),
+    override val nextList: MutableList<Node> = mutableListOf()
+) : Node
+
+data class SwitchNode(
+    override val id: String,
+    override val name: String,
+    override val type: NodeTypeEnum = NodeTypeEnum.SWITCH,
+    override val previousList: MutableList<Node> = mutableListOf(),
+    override val nextList: MutableList<Node> = mutableListOf(),
+    val tagMap: MutableMap<Node, String> = mutableMapOf()
+) : Node {
+    fun putTag(node: Node, tag: String) {
+        tagMap[node] = tag
+        addNextNode(node)
+    }
+}
+
+data class IfNode(
+    override val id: String,
+    override val name: String,
+    override val type: NodeTypeEnum = NodeTypeEnum.IF,
+    override val previousList: MutableList<Node> = mutableListOf(),
+    override val nextList: MutableList<Node> = mutableListOf(),
+    val trueNode: Node?,
+    val falseNode: Node?
+) : Node {
+    init {
+        if (trueNode != null) addNextNode(trueNode)
+        if (falseNode != null) addNextNode(falseNode)
+    }
+}
+
+data class SummaryNode(
+    override val id: String,
+    override val name: String,
+    override val type: NodeTypeEnum = NodeTypeEnum.SUMMARY,
+    override val previousList: MutableList<Node> = mutableListOf(),
+    override val nextList: MutableList<Node> = mutableListOf(),
+) : Node
