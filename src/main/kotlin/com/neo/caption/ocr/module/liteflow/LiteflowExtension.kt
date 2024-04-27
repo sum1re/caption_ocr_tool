e34bad935251053ee6814d2e85d7cf0fa30ed8dc
sum1re <sum1re@mail.sum1re.xyz>
2024-04-27 09:26:19 +0800

    feat: add extension for ELWrapper

--- src/main/kotlin/com/neo/caption/ocr/module/liteflow/LiteflowExtension.kt
+++ src/main/kotlin/com/neo/caption/ocr/module/liteflow/LiteflowExtension.kt
@@ -1,6 +1,28 @@
 package com.neo.caption.ocr.module.liteflow
 
+import com.yomahub.liteflow.builder.el.ELBus
+import com.yomahub.liteflow.builder.el.ELWrapper
+import com.yomahub.liteflow.builder.el.IfELWrapper
+import com.yomahub.liteflow.builder.el.IteratorELWrapper
+import com.yomahub.liteflow.builder.el.NodeELWrapper
+import com.yomahub.liteflow.builder.el.SwitchELWrapper
+import com.yomahub.liteflow.builder.el.ThenELWrapper
+import com.yomahub.liteflow.builder.el.WhileELWrapper
 import com.yomahub.liteflow.core.NodeComponent
 
 inline fun <reified T : BaseContext> NodeComponent.context(): T =
     this.getContextBean(T::class.java) ?: throw RuntimeException("context not found")
+
+fun NodeELWrapper.ternary(trueElWrapper: ELWrapper, falseElWrapper: ELWrapper): IfELWrapper =
+    ELBus.ifOpt(this, trueElWrapper, falseElWrapper)
+
+fun NodeELWrapper.ifTrue(trueElWrapper: ELWrapper): IfELWrapper = ELBus.ifOpt(this, trueElWrapper)
+
+fun ELWrapper.then(vararg elWrapper: ELWrapper): ThenELWrapper = ELBus.then(this, *elWrapper)
+
+fun NodeELWrapper.whileDo(elWrapper: ELWrapper): WhileELWrapper = ELBus.whileOpt(this).doOpt(elWrapper)
+
+fun <T : Enum<T>> NodeELWrapper.switch(vararg elWrapperWithTag: Pair<T, ELWrapper>): SwitchELWrapper =
+    ELBus.switchOpt(this).to(*elWrapperWithTag.map { it.second.tag(it.first.name) }.toTypedArray())
+
+fun NodeELWrapper.iterator(elWrapper: ELWrapper): IteratorELWrapper = ELBus.iteratorOpt(this).doOpt(elWrapper)
