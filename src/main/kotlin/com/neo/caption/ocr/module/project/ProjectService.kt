6cbbb25e6f874f6113524de5d3b8c2ff8a5a37db
sum1re <sum1re@mail.sum1re.xyz>
2024-07-06 21:36:13 +0800

    feat: add CRUD for project
    There are many modifications to this commit, including the CRUD for the project and associated classes, generate working or path fot the project.

--- src/main/kotlin/com/neo/caption/ocr/module/project/ProjectService.kt
+++ src/main/kotlin/com/neo/caption/ocr/module/project/ProjectService.kt
@@ -1,72 +1,213 @@
 package com.neo.caption.ocr.module.project
 
-import com.neo.caption.ocr.common.BadRequestException
-import com.neo.caption.ocr.common.CACHE_PROJECT
-import com.neo.caption.ocr.common.CACHE_TESS_CONFIG
-import com.neo.caption.ocr.common.CacheableService
-import com.neo.caption.ocr.common.ErrorCodeEnum
-import com.neo.caption.ocr.module.cv.toByteArray
-import com.neo.caption.ocr.module.file.FileChecksum
-import com.neo.caption.ocr.module.file.FileService
-import com.neo.caption.ocr.module.tesseract.AppTesseractService
-import com.neo.caption.ocr.module.tesseract.TesseractConfig
+import com.neo.caption.ocr.common.CHUNK_PREFIX
+import com.neo.caption.ocr.common.CommonProperties
+import com.neo.caption.ocr.common.CommonService
+import com.neo.caption.ocr.domain.toCaptionRow
+import com.neo.caption.ocr.domain.toProjectMetadata
+import com.neo.caption.ocr.module.liteflow.AstModelService
+import com.neo.caption.ocr.module.liteflow.FilterContext
+import com.neo.caption.ocr.module.liteflow.LiteflowChainId
+import com.neo.caption.ocr.module.liteflow.LiteflowService
+import com.neo.caption.ocr.module.liteflow.then
+import com.neo.caption.ocr.module.ocr.OcrService
 import com.neo.caption.ocr.service.CacheService
-import org.bytedeco.tesseract.TessBaseAPI
+import com.yomahub.liteflow.builder.el.ELBus.node
+import com.yomahub.liteflow.builder.el.ELWrapper
+import org.jetbrains.exposed.sql.JoinType
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.inList
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.update
 import org.opencv.core.Mat
 import org.opencv.videoio.VideoCapture
-import org.springframework.cache.annotation.CacheEvict
-import org.springframework.cache.annotation.Cacheable
+import org.opencv.videoio.Videoio
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
+import org.springframework.transaction.annotation.Transactional
+import java.math.BigDecimal
+import java.math.MathContext
+import java.math.RoundingMode
+import java.nio.file.Path
+import java.util.UUID
+import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.absolutePathString
-import kotlin.io.path.name
+import kotlin.io.path.createDirectory
+import kotlin.io.path.deleteIfExists
+import kotlin.io.path.deleteRecursively
+import kotlin.io.path.exists
+import kotlin.io.path.extension
+import kotlin.io.path.nameWithoutExtension
 
-@CacheableService
+@CommonService
+@Transactional
 class ProjectService(
-    private val fileService: FileService,
-    private val appTesseractService: AppTesseractService,
+    private val commonProperties: CommonProperties,
     private val cacheService: CacheService,
+    private val liteflowService: LiteflowService,
+    private val astModelService: AstModelService,
+    private val ocrService: OcrService,
 ) {
 
-    fun initialProject(fileChecksum: FileChecksum): Project = fileService.createWorkingDirectory().let {
-        Project(
-            id = it.name.substringAfterLast("_"),
-            workingDirectory = it,
-            name = fileChecksum.name,
-            hash = fileChecksum.hash,
-            extension = fileChecksum.extension,
+    fun generateWorkingDirectory(projectId: UUID): Path = projectId.toWorkingDirectory()
+
+    fun generateChunkPath(projectId: UUID, index: Int): Path =
+        projectId.toWorkingDirectory().resolve("${index.toChunkIndex()}.$CHUNK_PREFIX")
+
+    fun generateVideoPath(projectId: UUID, extension: String? = null): Path {
+        return if (extension.isNullOrEmpty()) ProjectMetadataTable.select(ProjectMetadataTable.extension)
+            .where { ProjectMetadataTable.projectId eq projectId }.singleOrNull()
+            ?.let { generateVideoPath(projectId, it[ProjectMetadataTable.extension]) }
+            ?: throw RuntimeException("Failed to generate project video path")
+        else projectId.toWorkingDirectory().resolve("video.$extension")
+    }
+
+    fun generateMatPath(projectId: UUID, matId: Long): Path = projectId.toWorkingDirectory().resolve("${matId}.webp")
+
+    fun generateMatUrl(projectId: UUID, matId: Long): String = "${projectId}/${matId}.webp"
+
+    // CRUD for Project
+    fun insertProject() {
+        ProjectTable.insertAndGetId {}.value.also {
+            it.toWorkingDirectory().createDirectory()
+            insertProjectMetadata(it)
+        }
+    }
+
+    fun findProjectDtoList(): List<ProjectDto> {
+        return ProjectTable.join(
+            otherTable = ProjectMetadataTable,
+            joinType = JoinType.INNER,
+            onColumn = ProjectTable.id,
+            otherColumn = ProjectMetadataTable.projectId,
         )
-    }.also { cacheService.put("$CACHE_PROJECT${it.id}", it) }
-
-    @Cacheable(key = "'$CACHE_TESS_CONFIG' + #p0")
-    fun initialTesseractConfig(projectId: String, tesseractConfig: TesseractConfig) = tesseractConfig
-
-    fun doOCR(projectId: String, mat: Mat) = ocr(appTesseractService.getTessBaseApi(projectId), mat)
-
-    fun doOCR(projectId: String, matList: List<Mat>) = appTesseractService.getTessBaseApi(projectId).run {
-        matList.map { ocr(this, it) }
+            .select(ProjectTable.id, ProjectTable.createdAt, ProjectMetadataTable.name)
+            .orderBy(ProjectTable.createdAt to SortOrder.DESC)
+            .map { ProjectDto(it[ProjectTable.id].value, it[ProjectMetadataTable.name]) }
     }
 
-    fun combineProjectVideo(projectId: String) {
-        val project = cacheService.get<Project?>("$CACHE_PROJECT$projectId", null)
-        require(project != null) { "unknown project: $projectId" }
-        fileService.combineChunk(project.workingDirectory, project.extension, project.hash)
+    fun findProjectByProjectId(projectId: UUID): Project =
+        ProjectTable.select(ProjectTable.astModelId, ProjectTable.ocrProfileId)
+            .where { ProjectTable.id eq projectId }
+            .singleOrNull()
+            ?.let {
+                Project(
+                    id = projectId,
+                    ocrProfile = ocrService.findOcrProfileByOcrProfileId(it[ProjectTable.ocrProfileId], true),
+                    astModel = astModelService.findAstModelById(it[ProjectTable.astModelId]),
+                    projectMetadata = findProjectMetadataByProjectId(projectId)
+                )
+            }
+            ?: throw RuntimeException("Failed to find project $projectId")
+
+    fun updateProject(projectId: UUID, astModelId: UUID? = null, ocrProfileId: UUID? = null) {
+        ProjectTable.update({ ProjectTable.id eq projectId }) {
+            if (astModelId != null) it[this.astModelId] = astModelId
+            if (ocrProfileId != null) it[this.ocrProfileId] = ocrProfileId
+        }
     }
 
-    fun openProjectVideo(projectId: String) =
-        VideoCapture(fileService.findVideoFile(projectId).absolutePathString())
-            .also { if (!it.isOpened) throw BadRequestException(ErrorCodeEnum.VIDEO_READ_ERROR) }
-
-    @CacheEvict(key = "'$CACHE_PROJECT' + #p0")
-    fun closeProject(projectId: String) {
-        appTesseractService.closeTessBaseApi(projectId)
+    @OptIn(ExperimentalPathApi::class)
+    fun deleteProject(projectId: UUID) {
+        liteflowService.remove(projectId)
+        liteflowService.removeChain(LiteflowChainId(projectId))
+        ProjectTable.deleteWhere { id eq projectId }
+        ProjectMetadataTable.deleteWhere { ProjectMetadataTable.projectId eq projectId }
+        CaptionRowTable.deleteWhere { CaptionRowTable.projectId eq projectId }
+        projectId.toWorkingDirectory().deleteRecursively()
     }
 
-    fun projectMetadata(projectId: String): ProjectMetadata {
-        TODO()
+    // CRUD for ProjectMetadata
+    fun insertProjectMetadata(projectId: UUID) {
+        ProjectMetadataTable.insert {
+            it[this.projectId] = projectId
+            it[this.name] = "tmp-$projectId"
+        }
     }
 
-    private fun ocr(tessBaseAPI: TessBaseAPI, mat: Mat): String {
-        tessBaseAPI.SetImage(mat.toByteArray(), mat.cols(), mat.rows(), mat.channels(), mat.cols())
-        return tessBaseAPI.GetUTF8Text().use { if (it.isNull) "" else it.string }
+    fun findProjectMetadataByProjectId(projectId: UUID): ProjectMetadata {
+        return ProjectMetadataTable.selectAll()
+            .where { ProjectMetadataTable.projectId eq projectId }
+            .singleOrNull()
+            ?.toProjectMetadata()
+            ?: throw RuntimeException("NotFound")
     }
+
+    fun updateProjectMetadataByVideoFile(projectId: UUID, videoFile: Path) {
+        require(videoFile.exists()) { "videoPath does not exist, ${videoFile.absolutePathString()}" }
+        val videoCapture = VideoCapture(videoFile.absolutePathString())
+        require(videoCapture.isOpened) { "Failed to access video file" }
+        ProjectMetadataTable.update({ ProjectMetadataTable.projectId eq projectId }) {
+            it[this.name] = videoFile.nameWithoutExtension
+            it[this.extension] = videoFile.extension
+            it[this.width] = videoCapture.get(Videoio.CAP_PROP_FRAME_WIDTH).toInt()
+            it[this.height] = videoCapture.get(Videoio.CAP_PROP_FRAME_HEIGHT).toInt()
+            it[this.fps] = videoCapture.get(Videoio.CAP_PROP_FPS)
+            it[this.totalFrames] = videoCapture.get(Videoio.CAP_PROP_FRAME_COUNT).toInt()
+            it[this.frameDuration] = BigDecimal("1000")
+                .divide(BigDecimal(fps.toString()), MathContext(5, RoundingMode.HALF_EVEN))
+        }
+        videoCapture.release()
+    }
+
+    // CRUD for CaptionRow
+    fun insertCaptionRow(captionRow: CaptionRow): Long {
+        return CaptionRowTable.insertAndGetId {
+            it[this.projectId] = captionRow.projectId
+            it[this.caption] = captionRow.caption
+            it[this.start] = captionRow.start
+            it[this.end] = captionRow.end
+        }.value
+    }
+
+    fun findCaptionRowByProjectId(projectId: UUID, pageable: Pageable): PageImpl<CaptionRow> {
+        val query = CaptionRowTable.selectAll()
+            .where { CaptionRowTable.projectId eq projectId }
+        val count = query.count()
+        return query.limit(pageable.pageSize, pageable.offset)
+            .orderBy(CaptionRowTable.start to SortOrder.ASC)
+            .map { it.toCaptionRow() }
+            .let { PageImpl(it, pageable, count) }
+    }
+
+    fun updateCaption(captionRowId: Long, caption: String) {
+        CaptionRowTable.update({ CaptionRowTable.id eq captionRowId }) {
+            it[this.caption] = caption
+        }
+    }
+
+    fun deleteCaptionRow(projectId: UUID, captionRowId: Long) {
+        CaptionRowTable.deleteWhere { this.id eq captionRowId }
+        generateMatPath(projectId, captionRowId).deleteIfExists()
+    }
+
+    fun deleteCaptionRow(projectId: UUID, captionRowIdList: Collection<Long>) {
+        CaptionRowTable.deleteWhere { (this.projectId eq projectId) and (this.id inList captionRowIdList.toSet()) }
+        captionRowIdList.map { generateMatPath(projectId, it) }.forEach { it.deleteIfExists() }
+    }
+
+    fun previewProjectFilter(projectId: UUID, mat: Mat, el: ELWrapper): PreviewResultDto {
+        try {
+            val chainId = LiteflowChainId(projectId)
+            liteflowService.createChain(chainId, el.then(node("isMatSingleChannel")))
+            val context = FilterContext(mutableListOf(), Mat())
+            val response = liteflowService.executeSync(chainId, context)
+            require(response.isSuccess) { throw response.cause }
+            val result: Mat = context.matStack.last().clone()
+            context.matStack.clear()
+            return PreviewResultDto(true, result.channels(), null, result)
+        } catch (t: Throwable) {
+            return PreviewResultDto(false, null, t.message ?: t::class.simpleName!!, null)
+        }
+    }
+
+    private fun UUID.toWorkingDirectory() = commonProperties.workingDirectory.resolve(this.toString())
+
+    private fun Int.toChunkIndex() = this.toString().padStart(5, '0')
 
 }
