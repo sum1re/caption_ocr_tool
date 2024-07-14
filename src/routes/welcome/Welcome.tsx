2c9ab4d4b61672727eeb8af92d721c7686dc5c92
sum1re <sum1re@mail.sum1re.xyz>
2024-07-14 21:56:04 +0800

    feat: init welcome page
    TODO: Complete welcome page

---
+++ src/routes/welcome/Welcome.tsx
@@ -0,0 +1,10 @@
+import { Unstable_Grid2 as Grid2 } from '@mui/material';
+import React from 'react';
+
+type WelcomeProps = {}
+
+export function Welcome({}: WelcomeProps): React.ReactElement {
+  return (
+    <Grid2 container></Grid2>
+  );
+}
