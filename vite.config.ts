4c20d2762a23a81e9620e0f3325867a623e1004a
sum1re <sum1re@mail.sum1re.xyz>
2024-09-21 14:40:54 +0800

    chore: configure server host and allow synthetic imports
    Set server host to 0.0.0.0 to allow access from other devices
    on the network.
    
    Enabled synthetic default imports to simplify importing
    modules.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+  },
   plugins: [react(), VitePWA({
     registerType: 'prompt',
     injectRegister: false,
@@ -33,4 +36,4 @@ export default defineConfig({
       type: 'module',
     },
   })],
-});
+})
