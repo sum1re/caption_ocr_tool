90b367dc59b32f3f227c3d9dd4eafdfd90801111
sum1re <sum1re@mail.sum1re.xyz>
2024-09-21 14:43:44 +0800

    feat: configure environment variables
    Added environment variables for API and media URLs.
    These variables are used by the application to interact
    with the backend services.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/react" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_MEDIA_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
