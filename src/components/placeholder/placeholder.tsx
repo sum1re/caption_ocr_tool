9eabaf6aa52dc11e22fcf071b6693be7ac3d1a8e
sum1re <sum1re@mail.sum1re.xyz>
2024-09-22 10:19:28 +0800

    feat(ui): add Placeholder component
    Adds a simple placeholder component using a CircularProgress
    component.  This component is not yet finished and requires
    further implementation.

---
+++ src/components/placeholder/placeholder.tsx
@@ -0,0 +1,9 @@
+import { CircularProgress } from '@mui/material'
+import { type ReactElement } from 'react'
+
+//TODO(Not yet finished)
+export function Placeholder(): ReactElement {
+  return (
+    <CircularProgress size={64} />
+  )
+}
