94b18e7dac2089b6dcdc972a304b0961e69740d9
sum1re <sum1re@mail.sum1re.xyz>
2026-08-18 08:33:09 +0800

    build(config): integrate tanstack router and optimize build settings
    - Integrate `@tanstack/router-plugin` to enable type-safe, auto-code-splitting routing for the application.
    - Enhance the production build process by adding asset compression via `vite-plugin-compression2`.
    - Implement modern build optimizations:
        - Set minifiers to use `oxc` and `lightningcss` for improved performance.
        - Configure specific build options like reporting compressed size and adjusting resource limits.

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,71 @@
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import compression from 'vite-plugin-compression2'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  appType: 'spa',
   server: {
     host: '0.0.0.0',
   },
-  plugins: [react(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+  plugins: [
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'Caption OCR Tool',
-      short_name: 'cocr',
-      description: 'A tool for extracting video caption.',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: 'Caption OCR Tool',
+        short_name: 'cocr',
+        description: 'A tool for extracting video caption.',
+        theme_color: '#ffffff',
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+    react(),
+    compression({
+      exclude: [/\.svg$/],
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    manifest: true,
+    target: 'baseline-widely-available',
+    cssCodeSplit: true,
+    modulePreload: {
+      polyfill: true,
     },
-  })],
+    minify: 'oxc',
+    cssMinify: 'lightningcss',
+    reportCompressedSize: true,
+
+    //build
+    sourcemap: false,
+    assetsInlineLimit: 4096,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        minifyInternalExports: true,
+      },
+    },
+  },
 })
