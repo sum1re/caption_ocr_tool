5c75812da05b83750d724b7893825a8d28e4a489
sum1re <sum1re@mail.sum1re.xyz>
2026-08-18 08:41:55 +0800

    refactor(common): remove LiteFlow exception usage from bad request exception
    - Removes the import for `com.yomahub.liteflow.exception.LiteFlowException` and the associated helper function, `throwLiteFlowException`.
    - This simplifies the `BadRequestException` class by eliminating direct dependency on external framework exception handling within this common utility file.

--- src/main/kotlin/com/neo/caption/ocr/common/BadRequestException.kt
+++ src/main/kotlin/com/neo/caption/ocr/common/BadRequestException.kt
@@ -1,14 +1,9 @@
 package com.neo.caption.ocr.common
 
-import com.yomahub.liteflow.exception.LiteFlowException
 import org.springframework.http.HttpStatus
 
 class BadRequestException(
     val code: ErrorCodeEnum,
     override val message: String = code.message,
-    val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST
+    val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST,
 ) : RuntimeException()
-
-fun throwLiteFlowException(code: ErrorCodeEnum, message: String = code.message): Nothing {
-    throw LiteFlowException(code.name, message)
-}
