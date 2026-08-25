a8fa757b57dfa4dd460ad440eaf9723fee706d99
sum1re <sum1re@mail.sum1re.xyz>
2026-08-25 22:10:01 +0800

    refactor(config): remove tesseract and ocr properties
    - Removes the dedicated `OCRProperties` and `TesseractProperties` data classes from `AppProperties.kt`.
    - This refactoring simplifies the definition of common application properties by centralizing configuration handling.
    - The specific configurations previously managed, such as pixel thresholds, SSIM, Tesseract engine mode, and UUID, are assumed to be handled or relocated elsewhere in the codebase.

--- src/main/kotlin/com/neo/caption/ocr/common/AppProperties.kt
+++ src/main/kotlin/com/neo/caption/ocr/common/AppProperties.kt
@@ -1,14 +1,11 @@
 package com.neo.caption.ocr.common
 
-import com.neo.caption.ocr.module.ocr.TesseractEngineEnum
-import com.neo.caption.ocr.module.ocr.TesseractPageSegModeEnum
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding
 import org.springframework.core.convert.converter.Converter
 import org.springframework.stereotype.Component
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.util.UUID
 
 @ConfigurationProperties(prefix = "cocr.cors")
 data class CorsProperties(
@@ -16,25 +13,6 @@ data class CorsProperties(
     val allowedHeader: List<String>,
     val allowedMethods: List<String>,
 )
-
-@ConfigurationProperties(prefix = "cocr.ocr")
-data class OCRProperties(
-    val minBlackPixelThreshold: Double,
-    val maxWhitePixelThreshold: Double,
-    val ssimThreshold: Double,
-    val invertThreshold: Double,
-)
-
-@ConfigurationProperties(prefix = "cocr.tesseract")
-data class TesseractProperties(
-    val uuid: UUID,
-    val ocrEngineMode: TesseractEngineEnum,
-    val pageSegMode: TesseractPageSegModeEnum,
-    val language: MutableList<String> = mutableListOf(),
-    val vectors: MutableList<TesseractVector> = mutableListOf(),
-) {
-    data class TesseractVector(val name: String, val value: String)
-}
 
 @ConfigurationProperties(prefix = "cocr.common")
 data class CommonProperties(
